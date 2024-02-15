a5833084884294d4d8ae94b5006265893180b82b
gosapp <gosapps86contact@gmail.com>
2024-02-15 05:17:10 -0300

    Add: se agrego unitTest getAuthentication

--- UnitTest/AuthServicesUnitTest.cs
+++ UnitTest/AuthServicesUnitTest.cs
@@ -1,10 +1,10 @@
 ﻿using Application.Interfaces;
+using Application.Models;
+using Application.UseCases;
+using Domain.Entities;
+using FluentAssertions;
+using FluentAssertions.Common;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace UnitTest
 {
@@ -28,9 +28,80 @@ namespace UnitTest
             var mockCommand = new Mock<IAuthCommands>();
             var mockQuery = new Mock<IAuthQueries>();
             var mockEncryptServices = new Mock<IEncryptServices>();
+
+            var service = new AuthServices(mockCommand.Object, mockQuery.Object, mockEncryptServices.Object);
+
+            Random rnd = new Random();
+            byte[] hash = new byte[64];
+            byte[] salt = new byte[128];
+            rnd.NextBytes(hash);
+            rnd.NextBytes(salt);
+
+            AuthReq authReq = new AuthReq
+            {
+                Email = "mail1@expresso.com",
+                Password = "Express0."
+            };
+
+            Authentication authentication = new Authentication
+            {
+                AuthId = Guid.NewGuid(),
+                Email = authReq.Email,
+                PasswordHash = hash,
+                PasswordSalt = salt,
+                UserId = 1
+            };
+
+            AuthResponseComplete authRespComplete = new AuthResponseComplete
+            {
+                AuthResponse = new AuthResponse
+                {
+                    Id = authentication.AuthId,
+                    Email = authReq.Email,
+                    UserId = 1
+                },
+                PasswordHash = hash,
+                PasswordSalt = salt
+            };
+            mockQuery.Setup(q => q.GetAuthByEmail(It.Is<string>(x => x.Equals("mail1@expresso.com")))).ReturnsAsync(authentication);
+
             //Act (Probar)
+            var result = await service.GetAuthentication(authReq);
 
             //Assert (Verificar)
+            result.AuthResponse.Id.Should().Be(authentication.AuthId);
+            result.AuthResponse.Email.Should().Be("mail1@expresso.com");
+            result.AuthResponse.UserId.Should().Be(1);
+            result.PasswordHash.Should().BeEquivalentTo<Byte>(hash);
+            result.PasswordSalt.Should().BeEquivalentTo<Byte>(salt);
         }
+
+        [Fact]
+        public async Task GetAuthenticationShouldReturnException()
+        {
+            //Arrange (Preparar)
+            var mockCommand = new Mock<IAuthCommands>();
+            var mockQuery = new Mock<IAuthQueries>();
+            var mockEncryptServices = new Mock<IEncryptServices>();
+
+            var service = new AuthServices(mockCommand.Object, mockQuery.Object, mockEncryptServices.Object);
+
+            AuthReq authReq = new AuthReq
+            {
+                Email = "noexisteexpresso.com",
+                Password = "Express0."
+            };
+
+            Authentication authentication = new Authentication();
+
+            mockQuery.Setup(q => q.GetAuthByEmail(It.Is<string>(x => x.Equals("noexisteexpresso.com")))).ReturnsAsync(() => null);
+
+            //Act (Probar)
+            var result = await service.GetAuthentication(authReq);
+
+            //Assert (Verificar)
+            result.Should().BeNull();
+        }
+
     }
 }
